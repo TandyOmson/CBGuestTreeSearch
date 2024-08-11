9d8cd8c8e29152f4cd582e8061ccfed4b0b64f4e
uccaat2 <uccaat2@login02.ib.young.ucl.ac.uk>
2024-08-11 18:41:16 +0100

    Modifications for getting amber working with MCTS on cluster

--- runscripts/chemsim_sge.sh
+++ runscripts/chemsim_sge.sh
@@ -8,13 +8,16 @@
 #$ -A UCL_chemM_Lee
 
 module unload gcc-libs
-module unload mpi
-module load amber/16/mpi/intel-2015-update2
+module load openblas/0.3.13-serial/gnu-10.2.0
 
-cd /home/uccaat2/Scratch/mcts_gaff/runscripts
 module load python/miniconda3/4.10.3
 source $UCL_CONDA_PATH/etc/profile.d/conda.sh
 conda activate chemts2
+
+module append-path PATH /home/uccaat2/nabc/bin
+module append-path PATH /shared/ucl/apps/amber-gcc/amber-20/bin
+
+cd /home/uccaat2/Scratch/mcts_gaff
 
 echo "Job start"
 
