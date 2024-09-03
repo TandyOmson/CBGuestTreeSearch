2e780c6efeabcb6e41c830690bffcad26778576a
uccaat2 <uccaat2@login02.ib.young.ucl.ac.uk>
2024-09-03 10:40:35 +0100

    Changes to sge runscripts

--- runscripts/mcts_sge.sh
+++ runscripts/mcts_sge.sh
@@ -6,6 +6,7 @@
 #$ -pe smp 40
 #$ -P Gold
 #$ -A UCL_chemM_Lee
+#$ -N mcts
 
 module unload gcc-libs
 module load openblas/0.3.13-serial/gnu-10.2.0
@@ -15,7 +16,12 @@ source $UCL_CONDA_PATH/etc/profile.d/conda.sh
 conda activate chemts2
 
 module append-path PATH /home/uccaat2/nabc/bin
-module append-path PATH /shared/ucl/apps/amber-gcc/amber-20/bin
+
+module append-path PATH /shared/ucl/apps/amber-gcc/amber-16-mpi/bin
+module append-path LD_LIBRARY_PATH /shared/ucl/apps/amber-gcc/amber-16-mpi/lib
+module append-path LD_RUN_PATH /shared/ucl/apps/amber-gcc/amber-16-mpi/lib
+export AMBERHOME=/shared/ucl/apps/amber-gcc/amber-16-mpi
+
 
 cd /home/uccaat2/Scratch/mcts_gaff
 
