ff45fbf9e0a1f4ccb2eb52a793123666da7bea2d
Andy Thomson <andrew.thomson25@virginmedia.com>
2024-11-05 10:28:52 +0800

    Made explicity Srrho contributions calculations. Added option to include small (artefactual) imaginary frequencies which are generally under 20cm**-1 to frequency calculations

--- reward/gaff_opt.py
+++ reward/gaff_opt.py
@@ -13,12 +13,18 @@ import math
 
 from rdkit import Chem
 
+"""SOME PHYSICAL CONSTANTS"""
+k = 1.380649E-23
+R = 8.314463
+h = 6.626070E-34
+nA = 602214076E-23
+
 """HELPER FUNCTIONS FOR GAFF OPTIMISATION METHODS"""
 
 @contextlib.contextmanager
-def my_temp_dir(parentdir, delete=False):
+def my_temp_dir(parentdir, delete=False, prefix=None):
     # Later python versions have a delete option on tempdir for debugging, creating one myself
-    temp_dir = tempfile.mkdtemp(dir=parentdir)
+    temp_dir = tempfile.mkdtemp(dir=parentdir, prefix=f"{str(prefix)}_")
     try:
         yield temp_dir
     finally:
@@ -112,6 +118,138 @@ def edit_resname(pdbfile, *constituents):
         f.writelines(lines)
     return
 
+""" FUNCTIONS FOR READING OUTPUTS """
+def extract_symmetry_and_rottemp(logfile):
+    lines = (i.strip().split() for i in open(logfile, "r"))
+
+    symmetry_number = 0
+    rt_x = 0
+    rt_y = 0
+    rt_z = 0
+
+    for count, line in enumerate(lines):
+        try:    
+            if " ".join(line[:2]) == "Rotational symmetry":
+                symmetry_number = int(line[4])
+            if " ".join(line[:2]) == "Rotational temperatures:":
+                rt_x = float(line[2])
+                rt_y = float(line[3])
+                rt_z = float(line[4])
+        except:
+            continue
+
+    return symmetry_number, rt_x, rt_y, rt_z
+
+def extract_mass(logfile):
+    lines = (i.strip().split() for i in open(logfile, "r"))
+
+    mass = 0
+    for count, line in enumerate(lines):
+        try:    
+            if line[0] == "Mass:":
+                mass = float(line[1])
+        except:
+            continue
+
+    # convert amu to kg
+    mass = mass*1.660539E-27
+
+    return mass
+
+def calc_S_vib(freqs, T=298.15, include_imag=False):
+    """ Vibrational component of entropy
+    """
+    
+    S = 0
+    for f in freqs:
+        if include_imag:
+            f = abs(float(f))
+        else:
+            f = float(f)
+        if f > 0:
+            f = f * 2.997925E10 # convert cm-1 to Hz
+            vt = h*f/k # vibrational temperature
+            i_mode = R*(vt/T/(math.exp(vt/T) - 1) - math.log(1 - math.exp(-vt/T)))
+            S += i_mode
+
+    # Convert to kcal/molK
+    S = S/4184
+    
+    return S
+
+def calc_E_vib(freqs, T=298.15, include_imag=False):
+    """ Vibrational component of energy
+    """
+    
+    E = 0
+    for f in freqs:
+        if include_imag:
+            f = abs(float(f))
+        else:
+            f = float(f)
+            
+        if f > 0:
+            f = f * 2.997925E10 # convert cm-1 to Hz
+            vt = h*f/k # vibrational temperature
+            i_mode = R*vt*(0.5 + 1/(math.exp(-vt/T) - 1))
+            E += i_mode
+
+    # Convert to kcal/mol
+    E = E/4184
+
+    return -E
+
+def calc_S_trans(mass, T=298.15, P=101325):
+    q = (((2*math.pi*mass*k*T)/h**2)**1.5)*((k*T)/P)
+    S = R*(math.log(q) + 2.5)
+
+    # Convert to kcal/molK
+    S = S/4184
+        
+    return S
+
+def calc_E_trans(T=298.15):
+    E = 1.5*R*T
+
+    # Convert to kcal/mol
+    E = E/4184
+    
+    return E
+
+def calc_S_elec(multiplicity=1):
+    S = R*(math.log(multiplicity))
+
+    # Convert to kcal/molK
+    S = S/4184
+    
+    return S
+
+def calc_E_elec():
+    return 0
+
+def calc_S_rot(symmetry_num, rt_x, rt_y, rt_z, T=298.15):
+    # characteristic rotational temperatures
+    #rt_x = (h**2)/(8*math.pi*I_x*k)
+    #rt_y = (h**2)/(8*math.pi*I_y*k)
+    #rt_z = (h**2)/(8*math.pi*I_z*k)
+
+    q = ((math.pi**0.5)/(symmetry_num)) * ((T**1.5)/((rt_x * rt_y * rt_z)**0.5))
+
+    S = R*(math.log(q) + 1.5)
+
+    # Convert to kcal/mol
+    S = S/4184
+
+    return S
+
+def calc_E_rot(T=298.15):
+    E = 1.5*R*T
+
+    # convert to kcal/mol
+    E = E/4184
+
+    return E
+
 class AmberMol():
     """ Stores the rdkit mol (which stores properties)
         In addition to objects required through GAFF calculations
@@ -172,10 +310,10 @@ class AmberCalculator():
         print("host optimised")
         
     # For gaff, the routine needs to keep the tempfiles of the guest to have the prmtop for the complex
-    def get_guest_complex_opt(self, comp, guest):
+    def get_guest_complex_opt(self, comp, guest, idx=None):
         """ optimises the guest, keeping the prmtop for the complex, then optimises the complex
         """
-        with my_temp_dir(parentdir=f"{self.outdir}", delete=False) as d:
+        with my_temp_dir(parentdir=f"{self.outdir}", delete=False, prefix=idx) as d:
             try:
                 orgdir = os.getcwd()
                 os.chdir(d)
@@ -208,7 +346,7 @@ class AmberCalculator():
             self.get_en_sander(ambermol, outfile=ambermol.files["min_out_sander"])
             self.nmode_nab(ambermol)
             #breakpoint()
-            self.get_nmode(ambermol, outfile=ambermol.files['nmode_out'])
+            self.get_thermo(ambermol, outfile=ambermol.files['nmode_out'])
 
             self.conf["chg_method"] = "gas"
 
@@ -219,7 +357,7 @@ class AmberCalculator():
             self.get_en_sander(ambermol, outfile=ambermol.files["min_out_sander"])
             self.nmode_nab(ambermol)
             #breakpoint()
-            self.get_nmode(ambermol, outfile=ambermol.files['nmode_out'])
+            self.get_thermo(ambermol, outfile=ambermol.files['nmode_out'])
 
         en = ambermol.en_dict["en"]
 
@@ -272,7 +410,7 @@ class AmberCalculator():
             chg_method = conf["chg_method"]
             ante_cmd.extend(["-c", chg_method])
 
-        ante_cmd.extend(["-nc", str(Chem.GetFormalCharge(ambermol.mol)), "-s", "2", "-pf", "yes", "-rn", ambermol.residue_name, "-at", "gaff2"])
+        ante_cmd.extend(["-nc", str(Chem.GetFormalCharge(ambermol.mol)), "-s", "2", "-pf", "yes", "-rn", ambermol.residue_name, "-at", "gaff"])
 
         sp.run(ante_cmd, stdout=sp.DEVNULL)
 
@@ -294,7 +432,7 @@ class AmberCalculator():
         ambermol.files["prmtop"] = f"{ambermol.name}.prmtop"
         ambermol.files["rst7"] = f"{ambermol.name}.rst7"
                
-        tleap_script = ["source leaprc.gaff2",
+        tleap_script = ["source leaprc.gaff",
                         f"loadamberparams {ambermol.files['frcmod']}",
                         f"{ambermol.residue_name} = loadmol2 {ambermol.files['mol2']}",
                         f"check {ambermol.residue_name}",
@@ -318,7 +456,7 @@ class AmberCalculator():
         complexambermol.files["rst7"] = f"{complexambermol.name}.rst7"
         complexambermol.residue_name = "COM"
 
-        tleap_script = ["source leaprc.gaff2"]
+        tleap_script = ["source leaprc.gaff"]
 
         tleap_script.extend([f"loadoff {ambermol.files['lib']}" for ambermol in constituents])
         tleap_script.extend([f"loadamberparams {ambermol.files['frcmod']}" for ambermol in constituents])
@@ -639,6 +777,74 @@ class AmberCalculator():
         Srrho = Srrho/4.184*T/1000
         ambermol.en_dict["Esrrho"] = Srrho
 
+        ambermol.en_dict["Ecorr"]  = ambermol.en_dict["en"] + ambermol.en_dict["Esrrho"]
+
+        return
+
+    def get_thermo(self, ambermol, outfile):
+        """ Retrieves thermochemical information from nmode output file
+        """
+        # Search for frequency blocks
+        lines = (i.strip().split() for i in open(outfile, "r"))
+
+        # Get vibrational mode frequencies
+        frequencies = []
+        for count, line in enumerate(lines):
+            try:
+                if " ".join(line[:2]) == "ff energy:":
+                    line = next(lines)
+
+                    # Take the specified number of normal modes
+                    for i in range(0, ambermol.mol.GetNumAtoms()*3):
+                        frequencies.append(float(line[1]))
+                        line = next(lines)
+                    
+            except StopIteration:
+                break
+            
+        # Valid frequencies will depend on number of degrees of freedom lost whether it is a complex, 6 for guest
+        valid_freqs = 6
+        try:
+            extra_frags = GetMolFrags(ambermol.mol, asMols=True) - 1
+        except:
+            extra_frags = 0
+        
+        valid_freqs += extra_frags*6
+
+        # Convert frequencies to appropraite units, account for loss of degrees of freedom
+        frequencies = [x for x in frequencies[valid_freqs:]]
+
+        sym_num , rt_x, rt_y, rt_z = extract_symmetry_and_rottemp(outfile)
+        mass = extract_mass(outfile)
+
+        S_vib, E_vib = calc_S_vib(frequencies, include_imag=self.conf["include_imaginary"]), calc_E_vib(frequencies, include_imag=self.conf["include_imaginary"])
+        S_trans, E_trans = calc_S_trans(mass), calc_E_trans()
+        S_elec, E_elec = calc_S_elec(), calc_E_elec()
+        S_rot, E_rot = calc_S_rot(sym_num, rt_x, rt_y, rt_z), calc_E_rot()
+        ambermol.en_dict["Strans"] = S_trans
+        ambermol.en_dict["Etrans"] = E_trans
+        ambermol.en_dict["Srot"] = S_rot
+        ambermol.en_dict["Erot"] = E_rot
+        ambermol.en_dict["Selec"] = S_elec
+        ambermol.en_dict["Eelec"] = E_elec
+        ambermol.en_dict["Svib"] = S_vib
+        ambermol.en_dict["Evib"] = E_vib
+
+        E_tot = E_vib + E_trans + E_elec + E_rot
+        S_tot = S_vib + S_trans + S_elec + S_rot
+        ambermol.en_dict["Einternal"] = E_tot
+        ambermol.en_dict["Sconfig"] = S_tot
+
+        #  kcal/mol
+        # This refers to thermal entropy (internal energy + kT)
+        Hrrho = E_tot + k*298.15*nA/4184
+        # This refers to the total thermochemical contribution (internal energy + kT - T*total entropy)
+        #Srrho = E_tot + k*298.15*nA/4184 - S_tot*298.15
+        # For some reason others use the thermochemical contribution only as -T*total_entropy
+        Srrho = -S_tot*298.15
+        
+        ambermol.en_dict["Hrrho"] = Hrrho
+        ambermol.en_dict["Esrrho"] = Srrho
         ambermol.en_dict["Ecorr"]  = ambermol.en_dict["en"] + ambermol.en_dict["Esrrho"]
 
         return
