821be15cbd4e6c1542391d3850870e07ea240668
Andrew Thomson <andyt@virginmedia.com>
2025-09-27 05:58:58 +0100

    Changed duplicate mol checking to use inchi

--- scratch_programs/dataset_prep/check_validity.py
+++ scratch_programs/dataset_prep/check_validity.py
@@ -1,5 +1,8 @@
 from rdkit import Chem
+from rdkit import RDLogger
 import sys
+
+RDLogger.DisableLog('rdApp.*')
 
 smis = [i.rstrip() for i in open(sys.argv[1], "r").readlines()]
 
@@ -8,7 +11,9 @@ too_big_count = 0
 unique_mol_count = 0
 duplicate_mol_count = 0
 
+dupes = []
 canon_smiles_map = []
+inchi_map = []
 for count, smi in enumerate(smis):
     print("SMILES string {} of {}, ({} duplicate, {} invalid, {} too big)".format(count, len(smis), duplicate_mol_count, invalid_mol_count, too_big_count), end="\r")
 
@@ -20,15 +25,26 @@ for count, smi in enumerate(smis):
         too_big_mol_count += 1    
         continue
     canon_smi = Chem.MolToSmiles(mol, isomericSmiles=True, kekuleSmiles=True, canonical=True)
-    if canon_smi in canon_smiles_map:
+
+    # Inchi is much better for finding duplicates than SMILES
+    inchi = Chem.MolToInchi(mol)
+    if inchi in inchi_map:
         duplicate_mol_count += 1
-        
+        dupes.append(mol)
     else:
         unique_mol_count += 1
         canon_smiles_map.append(canon_smi)
+        inchi_map.append(inchi)
 
 print("retained {}. ({} duplicate, {} invalid, {} too big)".format(len(canon_smiles_map), duplicate_mol_count, invalid_mol_count, too_big_count))
 
 with open(sys.argv[2], "w") as fw:
     for i in canon_smiles_map:
         fw.write(f"{i}\n")
+
+#from rdkit.Chem import AllChem        
+#writer = Chem.SDWriter("dupes.sdf")
+#for m in dupes:
+#    m_H = Chem.AddHs(m)
+#    AllChem.EmbedMolecule(m_H)
+#    writer.write(m_H)
