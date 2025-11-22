f941ff0338a68c02506c9789095717d6ba3a82a3
TCL <tcl25@IMD-658NV64.>
2025-11-22 15:16:15 +0000

    Minor edits to config

--- run_MCTS.sh
+++ run_MCTS.sh
@@ -18,7 +18,7 @@
 #chemtsv2 -c config/uniqueness_score_configs/MACCS_isomap_dist.yaml
 #chemtsv2 -c config/uniqueness_score_configs/MACCS_isomap_kde.yaml
 
-python3 chemtsv2/cli/run.py -c config/vina_plus_uniq/mcts_setting_03_10_25.yaml
+python3 chemtsv2/cli/run.py -c config/vina_plus_uniq/22_11_25_quadrant_4.yaml
 
 
 
